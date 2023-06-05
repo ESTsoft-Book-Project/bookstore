2245cdc0f5b2e9f48608ac07d148a44a3e9ca352
kim-ido <kimido987@gmail.com>
2023-06-05 15:59:47 +0900

    feat(signup): Update settings.py add appname in installed_apps

--- users/models.py
+++ users/models.py
@@ -1,49 +1,5 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
-        if not username:
-            raise ValueError("The Username field must be set")
-        
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True')
-        
-        return self.create_user(username, password, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    email = models.EmailField(blank=True)
-    password = models.CharField(max_length=128)
-    groups = models.ManyToManyField('auth.Group', blank=True, related_name='user_set', related_query_name='user')
-    user_permissions = models.ManyToManyField('auth.Permission', blank=True, related_name='user_set', related_query_name='user')
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    last_login = models.DateTimeField(null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
-    
-    objects = UserManager()
-
-    class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
-
-    def __str__(self):
-        return self.username
+# 기존 유저 모델을 상속받아 사용자 정의 모델을 생성합니다.
+class CustomUser(AbstractUser):
+    pass
