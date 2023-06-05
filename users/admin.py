6df9d505d02b315a9499931046ca8310215be14b
taencoding <xodms512@gmail.com>
2023-06-05 19:01:59 +0900

    refactor(signup): Connect url and modify model

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import UserProfile
-
-admin.site.register(UserProfile)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import AbstractUser
 
 # 기존 유저 모델을 상속받아 사용자 정의 모델을 생성합니다.
-class CustomUser(AbstractUser):
-    pass
+
