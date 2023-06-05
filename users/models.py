2f9e289ffa7916fdfa171df58138320c5da2ebc0
nohheeyeon <scarletberries@naver.com>
2023-06-05 14:15:24 +0900

    Import default user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# 기존 유저 모델을 상속받아 사용자 정의 모델을 생성합니다.
+class CustomUser(AbstractUser):
+    pass
