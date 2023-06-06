e8ea395be56f03c00723bf58e2b8577d16d1540e
taencoding <xodms512@gmail.com>
2023-06-07 00:43:16 +0900

    feat(signup): Update redirect

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
+            return redirect('users:signin')
     else:
         form = SignUpForm()
     
