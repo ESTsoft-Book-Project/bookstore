d340dc52011fa45e763c3640aababf18287096ec
ChoiWheatley <chltmdgus604@naver.com>
2023-06-07 02:20:32 +0000

    fix(username): admin and forms depends on User

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
