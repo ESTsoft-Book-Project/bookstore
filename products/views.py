b401365fd3cd3ddd18b5967bb867fcbdf7574f15
ian <ian.kim.dev@gmail.com>
2023-06-13 18:27:54 +0900

    feat(products): Add products models, forms, views, urls

---
+++ products/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .forms import ProductForm
+from .models import Product
+from django.shortcuts import render, get_object_or_404
+
+def book_list(request):
+    books = Product.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+
+def book_detail(request, handle):
+    book = get_object_or_404(Product, handle=handle)
+    return render(request, 'book_detail.html', {'book': book})
+
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProductForm()
+    
+    return render(request, 'create_product.html', {'form': form})
+
