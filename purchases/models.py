5cd6cce00a058475a0bc123ab7f421f495ca489a
ian <ian.kim.dev@gmail.com>
2023-06-13 19:06:05 +0900

    refactor(purchases): Modify purchases models

--- purchases/models.py
+++ purchases/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.conf import settings
+from products.models import Product
 
 class Purchase(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    purchase_date = models.DateTimeField(auto_now_add=True)
-
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
     
