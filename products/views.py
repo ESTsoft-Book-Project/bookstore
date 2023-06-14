83473ad74d93c1cb3b82dd29a04afb31be223c81
allen9535 <allen9535@naver.com>
2023-06-14 16:41:52 +0900

    feat(product): Add create function to product app

--- products/views.py
+++ products/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .forms import ProductForm
 from .models import Product
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from slugify import slugify
+import json
 
 def book_list(request):
     books = Product.objects.all()
@@ -14,12 +17,17 @@ def book_detail(request, handle):
 
 
 def create_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
+    if request.method == "POST":
+        request_data = json.loads(request.body)
+        request_data["handle"] = slugify(request_data["name"])
+        form = ProductForm(request_data)
         if form.is_valid():
-            form.save()
+            product = form.save(commit=False)
+            product.user = request.user
+            product.save()
+            return JsonResponse({"message": "신규 도서 등록이 완료되었습니다."})
+        else:
+            print(form.errors.as_json())
+            return JsonResponse({"message": "오류가 발생했습니다."})
     else:
-        form = ProductForm()
-    
-    return render(request, 'create_product.html', {'form': form})
-
+        return render(request, 'create_product.html')
