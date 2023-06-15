3071defb4a3314213c89c7a8c47a0d880a9d5c47
ChoiWheatley <chltmdgus604@naver.com>
2023-06-15 11:52:47 +0000

    dep(crispy): Del dependency, we only use bootstrap

--- core/settings.py
+++ core/settings.py
@@ -130,8 +130,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Tell Django to use our custom user model instead of the built-in User model.
 AUTH_USER_MODEL = "users.User"
-
-# https://github.com/django-crispy-forms/crispy-bootstrap5
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
-CRISPY_TEMPLATE_PACK = "bootstrap5"
