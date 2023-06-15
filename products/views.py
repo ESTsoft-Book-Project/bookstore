3fc4f1ac286142c2eef78dd04debc8a74bbff5b3
kim-ido <kimido987@gmail.com>
2023-06-15 23:08:17 +0900

    feat(product): Create update_product page

--- products/views.py
+++ products/views.py
@@ -12,11 +12,9 @@ def book_list(request):
     books = Product.objects.all()
     return render(request, 'book_list.html', {'books': books})
 
-
 def book_detail(request, handle):
     book = get_object_or_404(Product, handle=handle)
     return render(request, 'book_detail.html', {'book': book})
-
 
 @login_required(login_url="/users/signin/")
 def create_product(request):
@@ -33,3 +31,24 @@ def create_product(request):
             return JsonResponse({"message": form.errors.as_json()})
     else:
         return render(request, 'create_product.html')
+    
+@login_required(login_url="/users/signin/")
+def update_product(request, handle):
+    book = get_object_or_404(Product, handle=handle)
+
+    if request.method == "PATCH":
+        request_data = json.loads(request.body)
+        request_data["handle"] = slugify(request_data["name"])
+        form = ProductForm(request_data, instance=book)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.user = request.user
+            product.save()
+            return JsonResponse({"message": "도서 정보가 수정되었습니다."}, status = 200)
+        else:
+            return JsonResponse({"message": form.errors.as_json()}, status = 400)
+    if not Product.objects.filter(handle=handle).exists():
+        return JsonResponse({"message": "존재하지 않는 상품입니다."}, status = 404)
+    else:
+        form = ProductForm(instance=book)
+        return render(request, "update_product.html")
