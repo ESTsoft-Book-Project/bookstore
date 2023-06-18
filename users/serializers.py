adc45544d2d20f7acca8121d7d03f5e5f03b91b0
Im dong seong <ririro238@gmail.com>
2023-06-18 22:33:30 +0900

    feat(user_signup,in,out): serializers add

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "nickname", "password1", "password2"]
+    
+    def create(self, validated_data):
+        password1 = validated_data.pop("password1")
+        password2 = validated_data.pop("password2")
+        if password1 != password2:
+            raise serializers.ValidationError("비밀번호가 일치하지 않습니다.")
+        user = User.objects.create_user(**validated_data, password=password1)
+        return user
+    
+
