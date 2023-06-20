893ffbcc81d99e81923b899ebd0f9d7f28668b00
ChoiWheatley <chltmdgus604@naver.com>
2023-06-20 06:47:01 +0000

    feat(create_product): add form `stock`

--- products/forms.py
+++ products/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'handle', 'price']
+        fields = ['name', 'handle', 'price', 'stock']
