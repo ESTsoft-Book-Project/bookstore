c4b5f06049b97b8db786343f64ad6ffa79216704
nohheeyeon <scarletberries@naver.com>
2023-06-24 02:46:28 +0900

    feat(forms): add CommentForm for creating comments

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Product
+from .models import Product, Comment
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'image', 'stock']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
