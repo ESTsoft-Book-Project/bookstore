19ee82b1325840477b7e562c61bd83104950b5b9
taencoding <xodms512@gmail.com>
2023-06-24 14:38:50 +0900

    feat(carts): Add order_detail

--- carts/urls.py
+++ carts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import cart_add, cart_list, cart_update, cart_delete, product_list
+from .views import cart_add, cart_list, cart_update, cart_delete, product_list, order_detail, order_product_detail
 
 app_name = 'carts'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('add/', cart_add, name='cart_add'),
     path('update/', cart_update, name='cart_update'),
     path('delete/', cart_delete, name='cart_delete'),
+    path('order_detail/', order_detail, name='order_detail'),
+    path('order_detail/order_product_detail/', order_product_detail, name='order_product_detail'),
 ]
