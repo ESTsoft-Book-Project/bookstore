929e9f32c35b6f2dfaf788b40d7bdc7c2d95f8d6
ian <ian.kim.dev@gmail.com>
2023-06-24 21:01:05 +0900

    chore(purchases): Modify purchase URLs to use stripe prefix

--- purchases/urls.py
+++ purchases/urls.py
@@ -3,9 +3,9 @@ from . import views
 app_name = 'purchases'
 
 urlpatterns = [
-    path('start/', views.purchase_start_view, name="start"),
-    path('success/', views.purchase_success_view, name='success'),
-    path('stopped/', views.purchase_stopped_view, name='stopped'),
+    path('stripe_start/', views.stripe_start, name="stripe_start"),
+    path('stripe_success/', views.stripe_success, name='stripe_success'),
+    path('stripe_stopped/', views.stripe_stopped, name='stripe_stopped'),
     path('orders/', views.purchase_order_view, name='orders'),
     path('kakaopay_start/', views.kakaopay_start_view, name='kakaopay_start'),
     path('kakaopay_success/', views.kakaopay_success_view, name='kakaopay_success'),
