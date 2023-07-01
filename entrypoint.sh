ef5407228b2b82c711868be62b07a5104d7e391a
ian <ian.kim.dev@gmail.com>
2023-07-01 20:37:43 +0900

    build: Add collectstatic in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ APP_PORT=${PORT:-8000}
 
 cd /app/
 
+/opt/venv/bin/python manage.py collectstatic --noinput
 /opt/venv/bin/gunicorn core.wsgi:application --bind "0.0.0.0:${APP_PORT}"
 nginx -g "daemon off;"
