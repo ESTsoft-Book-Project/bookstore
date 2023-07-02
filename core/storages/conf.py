f3aaf1b63d16a8985f6e729e1b4b177213bf9068
ChoiWheatley <chltmdgus604@naver.com>
2023-07-02 12:22:37 +0000

    fix(s3): static urls are now s3 url

--- core/storages/conf.py
+++ core/storages/conf.py
@@ -6,6 +6,7 @@ AWS_S3_ADDRESSING_STYLE = "virtual"
 
 AWS_STORAGE_BUCKET_NAME=config("AWS_STORAGE_BUCKET_NAME", default="estsoft-ormi-bookstore")
 AWS_S3_REGION_NAME="ap-northeast-2"
+AWS_S3_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 AWS_DEFAULT_ACL="public-read"
 AWS_S3_USE_SSL=True
